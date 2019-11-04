48882dadb601e47b6d8612646b864ba8ac3b6f75
Michael Cooper <macoop2363@gmail.com>
2019-11-04 15:48:47 -0700

    Add test for link to a user's addresses page on the user profile page

--- spec/features/users/profile_spec.rb
+++ spec/features/users/profile_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'As a registered user' do
   describe 'when I visit my profile page' do
-    xit 'can see all profile data on the page except the password' do
+    it 'can see all profile data on the page except the password' do
       user = User.create(
         name: 'Bob',
         email: 'bob@email.com',
@@ -25,10 +25,6 @@ RSpec.describe 'As a registered user' do
 
       within '#user-info' do
         expect(page).to have_content('Name: Bob')
-        expect(page).to have_content('Address: 123 Main')
-        expect(page).to have_content('City: Denver')
-        expect(page).to have_content('State: CO')
-        expect(page).to have_content('Zip: 80233')
         expect(page).to have_content('Email: bob@email.com')
       end
     end
@@ -57,7 +53,7 @@ RSpec.describe 'As a registered user' do
       expect(current_path).to eq('/profile/edit')
     end
 
-    xit 'has a link to user order page' do
+    it 'has a link to user order page' do
       user = User.create(
         name: 'Bob',
         email: 'bob@email.com',
@@ -79,6 +75,24 @@ RSpec.describe 'As a registered user' do
       end
 
       expect(current_path).to eq('/profile/orders')
+    end
+
+    it 'has a link to a users address page' do
+      user = User.create(
+        name: 'Bob',
+        email: 'bob@email.com',
+        password: 'secure'
+      )
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit profile_path
+
+      within '#user-addresses' do
+        click_link 'My Addresses'
+      end
+
+      expect(current_path).to eq('/profile/addresses')
     end
   end
 end
