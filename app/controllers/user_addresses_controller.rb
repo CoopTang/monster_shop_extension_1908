1f74f240926eb9e5592ca9b75d82f0622c3195ba
Michael Cooper <macoop2363@gmail.com>
2019-11-04 17:45:45 -0700

    Add actions for user address show, new, and edit

--- app/controllers/user_addresses_controller.rb
+++ app/controllers/user_addresses_controller.rb
@@ -2,4 +2,14 @@ class UserAddressesController < ApplicationController
   def index
     @addresses = current_user.addresses
   end
+
+  def show
+    @address = Address.find(params[:address_id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
 end
