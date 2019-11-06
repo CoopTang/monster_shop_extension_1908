cc4a717e28682d69cc968ceccf7ae33aadbdf83c
Michael Cooper <macoop2363@gmail.com>
2019-11-05 23:18:28 -0700

    Add edit and update actions and renamed action to better reflect its purpose

--- app/controllers/user_orders_controller.rb
+++ app/controllers/user_orders_controller.rb
@@ -8,7 +8,18 @@ class UserOrdersController < ApplicationController
     render_404 unless @order
   end
 
+  def edit
+    @order = Order.find(params[:id])
+  end
+
   def update
+    order = Order.where(id: params[:id], user_id: current_user.id).first
+    order.update(order_params)
+    flash[:notice] = 'Shipping address has been updated!'
+    redirect_to "/orders/#{params[:id]}"
+  end
+
+  def cancel
     order = Order.where(id: params[:id], user_id: current_user.id).first
     order.update(status: 'Cancelled')
     order.item_orders.each do |item_order|
@@ -17,5 +28,11 @@ class UserOrdersController < ApplicationController
     end
     flash[:notice] = 'Order has been cancelled!'
     redirect_to profile_path
+  end
+
+  private 
+  
+  def order_params
+    params.permit(:name, :address_id)
   end
 end
