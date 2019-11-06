0203c4cf3b9e70e0a65bf377dcebb4ee74a8128b
Michael Cooper <macoop2363@gmail.com>
2019-11-06 00:42:46 -0700

    Add user order edit functionality for site admin

--- app/controllers/admin/user_orders_controller.rb
+++ app/controllers/admin/user_orders_controller.rb
@@ -4,6 +4,17 @@ class Admin::UserOrdersController < Admin::BaseController
     @order = Order.find(params[:order_id])
   end
 
+  def edit
+    @order = Order.find_by(id: params[:order_id], user_id: params[:user_id])
+  end
+
+  def update
+    order = Order.where(id: params[:order_id], user_id: params[:user_id])
+    order.update(order_params)
+    flash[:notice] = 'Shipping address has been updated!'
+    redirect_to "/admin/users/#{params[:user_id]}/orders/#{params[:order_id]}"
+  end
+
   def cancel
     order = Order.where(id: params[:order_id], user_id: params[:user_id]).first
     order.update(status: 'Cancelled')
@@ -12,7 +23,12 @@ class Admin::UserOrdersController < Admin::BaseController
       item_order.update_attributes(status: 'Unfulfilled')
     end
     flash[:notice] = 'Order has been cancelled!'
-    redirect_to admin_users_path(params[:user_id])
+    redirect_to "/admin/users/#{params[:user_id]}"
   end
 
+  private 
+  
+  def order_params
+    params.permit(:name, :address_id)
+  end
 end
